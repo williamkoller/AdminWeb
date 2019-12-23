ad5c6fb08a5e7efaa5df92aba721f19add1132f7
William Koller <wkoller25@gmail.com>
2019-12-23 10:54:55 -0300

    rename file

---
+++ src/connection.php
@@ -0,0 +1,9 @@
+<?php
+
+mysqli_report(MYSQLI_REPORT_ERROR);
+$server = 'localhost';
+$user = 'root';
+$pass = '210689';
+$db = 'admin_web_php';
+$conn = new mysqli($server, $user, $pass, $db);
+
---
+++ src/render.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * @param $content
+ * @param $template
+ * @param array $data
+ * @return mixed
+ */
+function render($content, $template, array $data = [])
+{
+    $content =  __DIR__ . '/../templates/' . $content . '.tpl.php';
+    return include __DIR__ . '/../templates/' . $template . '.tpl.php';
+}
