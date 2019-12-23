2a5e5226aaae7b93e4c11289920a2f8c52bc3661
William Koller <wkoller25@gmail.com>
2019-12-22 22:29:29 -0300

    trabalhando com rotas amigaveis, usando function

---
+++ admin/routes.php
@@ -0,0 +1,9 @@
+<?php
+
+if (resolve('/admin')) {
+    echo 'Administração';
+} elseif (resolve('/admin/pages')){
+    echo 'Administração de Páginas';
+} else {
+    echo 'Página não encontrada';
+}
