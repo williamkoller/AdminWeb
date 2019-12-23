1991822dee11c85da39ace841f84d6e63d4b22b7
William Koller <wkoller25@gmail.com>
2019-12-23 00:54:07 -0300

    página de bem-vindo

--- templates/site.tpl.php
+++ templates/site.tpl.php
@@ -1,1 +1,16 @@
-<?php
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Bem vindo ao William Dev </h1>
+    <?php include $content;?>
+
+    <p><small>2019 - Todos os direitos reservados</small></p>
+</body>
+</html>
