e7a02365daa92e3a66dacbdf49485ba399903901
William Koller <wkoller25@gmail.com>
2019-12-22 22:29:54 -0300

    rotas amigaveis com function

--- bootstrap.php
+++ bootstrap.php
@@ -1,7 +1,22 @@
 <?php
 
-$path = $_SERVER['PATH_INFO'] ?? '/';
+function resolve($route){
+    $path = $_SERVER['PATH_INFO'] ?? '/';
+    $route = '/^' . str_replace('/','\/',$route) . '$/';
 
-if($path === '/'){
+    if (preg_match($route, $path, $params)){
+        return $params;
+    }
+    return false;
+}
+
+//var_dump(resolve('/'));
+//var_dump(resolve('/admin'));
+
+
+
+if (resolve('/admin/?(.*)')){
+    require __DIR__ . '/admin/routes.php';
+} elseif (resolve('/(.*)')) {
     require __DIR__ . '/site/routes.php';
 }
