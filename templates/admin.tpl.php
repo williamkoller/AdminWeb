f1eb9b04eb4658278d23c415d74d03784d103cf4
William Koller <wkoller25@gmail.com>
2019-12-24 14:58:26 -0300

    adicionando css na página

--- templates/admin.tpl.php
+++ templates/admin.tpl.php
@@ -1,14 +1,49 @@
-<!doctype html>
-<html lang="en">
+
+<!DOCTYPE html>
+<html lang="pt-br">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <!-- Meta tags Obrigatórias -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" href="/css/style.css">
     <title>Painel Administrativo</title>
 </head>
-<body>
-    <h1>Administração</h1>
-    <?php  $content;?>
+<body class="d-flex flex-column">
+    <div id="header">
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <a href="" class="navbar-brand">Admin</a>
+            <span class="navbar-text">Painel Administrativo</span>
+        </nav>
+    </div>
+    <div id="main">
+        <div class="row">
+            <div class="col">
+                <ul id="main-menu" class="nav flex-column nav-pills bg-secondary text-white p-2">
+                    <li class="nav-item">
+                        <span href="" class="nav-link text-white-50"><small>MENU</small></span>
+                    </li>
+                    <li class="nav-item">
+                        <a href="" class="nav-link active">Páginas</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="" class="nav-link">Usuários</a>
+                    </li>
+                </ul>
+            </div>
+            <div id="content" class="col-10">
+                <?php include $content;?>
+
+            </div>
+        </div>
+
+    </div>
+<!-- JavaScript (Opcional) -->
+<!-- jQuery primeiro, depois Popper.js, depois Bootstrap JS -->
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
 </body>
 </html>
