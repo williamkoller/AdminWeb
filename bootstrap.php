6753bcba5944f0f14bdfb363bb1f8838f5a823d9
William Koller <wkoller25@gmail.com>
2019-12-24 14:57:44 -0300

    renomeando src

--- bootstrap.php
+++ bootstrap.php
@@ -1,9 +1,9 @@
 <?php
 include __DIR__ . 'config.php';
-include __DIR__ . '/scr/error_handler.php';
-include __DIR__ . '/scr/resolve-route.php';
-include __DIR__ . '/scr/render.php';
-include __DIR__ . '/scr/connection.php';
+include __DIR__ . '/src/error_handler.php';
+include __DIR__ . '/src/resolve-route.php';
+include __DIR__ . '/src/render.php';
+include __DIR__ . '/src/connection.php';
 
 if (resolve('/admin/?(.*)')){
     require __DIR__ . '/admin/routes.php';
