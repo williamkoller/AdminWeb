3810f110f5d48c13fb08b9e5c582ce8e0cf54fb2
William Koller <wkoller25@gmail.com>
2019-12-25 03:12:10 -0300

    adicionando index

--- templates/admin/pages/index.tpl.php
+++ templates/admin/pages/index.tpl.php
@@ -1,5 +1,18 @@
-<h3>Administração de páginas</h3>
-
-
-//listagem
-<a href="/admin">Ir para home</a>
+<h3 class="mb-5">Administração de páginas</h3>
+<table class="table table-hover">
+    <thead>
+    <tr>
+        <th>#</th>
+        <th>Título</th>
+        <th></th>
+    </tr>
+    </thead>
+    <tbody>
+    <tr>
+        <td>1</td>
+        <td><a href="/admin/pages/1">Página inicial</a></td>
+        <td class="text-right"><a href="/admin/pages/1" class="btn btn-primary btn-sm">Ver</a></td>
+    </tr>
+    </tbody>
+</table>
+<a href="/admin/pages/create" class="btn btn-secondary btn-sm">Novo Registro</a>
