3d4a8de042608e8ede225f477c8326f8d1aa998c
William Koller <wkoller25@gmail.com>
2019-12-25 03:11:10 -0300

    finalizando formulário

--- templates/admin/pages/create.tpl.php
+++ templates/admin/pages/create.tpl.php
@@ -1,4 +1,26 @@
 <h3>Administração de páginas</h3>
+<form action="" method="post">
+    <div class="form-group">
+        <label for="pagesTitle">Título</label>
+        <input name="title" id="pagesTitle" type="text" class="form-control" placeholder="Aqui vai o título da página ..." required>
+    </div>
 
-// criação de página
-<a href="/admin">Ir para home</a>
+    <div class="form-group">
+        <label for="pagesUrl">URL</label>
+        <div class="input-group">
+            <div class="input-group-prepend">
+                <span class="input-group-text">/</span>
+            </div>
+            <input name="url" id="pagesUrl" type="text" class="form-control" placeholder="URL amigável, deixe em branco para informar a página inicial..">
+        </div>
+    </div>
+
+    <div class="form-group">
+        <input id="pagesBody" type="hidden" name="body">
+        <trix-editor input="pagesBody"></trix-editor>
+    </div>
+
+    <button type="submit" class="btn btn-primary btn-sm">Salvar</button>
+</form>
+<hr>
+<a href="/admin/pages" class="btn btn-secondary btn-sm">Voltar</a>
