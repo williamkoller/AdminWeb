f08717730e9d7cdb9da9c5374d4547143a65e87b
William Koller <wkoller25@gmail.com>
2019-12-27 17:17:31 -0300

    página de listagem de usuário

--- templates/admin/users/index.tpl.php
+++ templates/admin/users/index.tpl.php
@@ -1,3 +1,31 @@
-<?php
+<h3 class="md-5">Administração de usuários</h3>
 
-echo '<h1>Index Users</h1>';
+<table class="table table-hover">
+  <thead>
+    <tr>
+      <th>#</th>
+      <th>email</th>
+      <th></th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>1</td>
+      <td>william@gmail.com</td>
+      <td class="text-right">
+        <a href="/admin/users/1" class="btn btn-primary btn-sn">Ver</a>
+      </td>
+    </tr>
+    <tr>
+      <td>2</td>
+      <td>wkoller@gmail.com</td>
+      <td class="text-right">
+        <a href="/admin/users/2" class="btn btn-primary btn-sn">Ver</a>
+      </td>
+    </tr>
+  </tbody>
+
+</table>
+
+<a href="/admin/users/create" class="btn btn-secondary btn-sm">Novo Registro</a>
+
