e530cb853d88253306a6ebbe2af8876a40d4564b
William Koller <wkoller25@gmail.com>
2019-12-27 19:22:41 -0300

    página de edição de usuário

--- templates/admin/users/edit.tpl.php
+++ templates/admin/users/edit.tpl.php
@@ -1,13 +1,13 @@
 <h3 class="md-5">Editar usuário</h3>
 
-<form method="post" action="/admin/users">
+<form method="post">
   <div class="form-group">
     <label for="usersEmail">Email</label>
-    <input id="usersEmail" type="email" name="email" value="wkoller25@gmail.com" class="form-control">
+    <input id="usersEmail" type="email" name="email" value="<?php echo $data['user']['email'];?>" class="form-control">
   </div>
   <div class="form-group">
     <label for="usersPassword">Password</label>
-    <input id="usersPassword" type="password" name="password" value="<h4>*****</h4>" class="form-control">
+    <input id="usersPassword" type="password" name="password" value="" placeholder="Informe sua senha aqui ..." class="form-control">
   </div>
 
   <button type="submit" class="btn btn-primary btn-sm">Salvar</button>
