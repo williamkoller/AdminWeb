e444fc799fea333a6e10ffb1c1bcea4537a2fc43
William Koller <wkoller25@gmail.com>
2019-12-28 01:46:55 -0300

    layout do site

--- templates/site.tpl.php
+++ templates/site.tpl.php
@@ -1,16 +1,35 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Meu site</title>
+    <link rel="stylesheet" href="/resources/pnotify/pnotify.custom.min.css">
+    <link rel="stylesheet" href="/site.css">
 </head>
 <body>
-    <h1>Bem vindo ao William Dev </h1>
-    <?php include $content;?>
+<header id="header">
+    <h1>Bem vindo ao meu site</h1>
+</header>
 
-    <p><small>2019 - Todos os direitos reservados</small></p>
+<ul id="nav">
+    <?php foreach ($data['pages'] as $item): ?>
+        <li><a href="/<?php echo $item['url']?>"><?php echo $item['title']?></a></li>
+    <?php endforeach; ?>
+    <li><a href="/contato">Contato</a></li>
+</ul>
+
+<main id="content" class="container">
+    <?php include $content; ?>
+</main>
+
+<p id="footer"><small><?php echo date('Y'); ?> - todos os direitos reservados</small></p>
+
+<script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+<script src="/resources/pnotify/pnotify.custom.min.js"></script>
+<script>
+    <?php flash(); ?>
+</script>
 </body>
 </html>
