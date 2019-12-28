84214c50aabe4cb72f0378f1ab5e179023c24f1d
William Koller <wkoller25@gmail.com>
2019-12-28 01:45:09 -0300

    ajustando se usuário está autenticado

--- src/auth.php
+++ src/auth.php
@@ -1,1 +1,24 @@
 <?php
+
+function get_user()
+{
+    return $_SESSION['auth'] ?? null;
+}
+
+function auth_protection()
+{
+    $user = get_user();
+
+    if (!$user && !resolve('/admin/auth.*')){
+        header('Location:  /admin/auth/login');
+        die();
+    }
+}
+
+function logout()
+{
+    unset($_SESSION['auth']);
+    flash('Você se desconectou', 'success');
+    header('Location: /admin/auth/login');
+    die();
+}
