5c219693827a5e1c62b63329ff87ab2afd0e93c0
William Koller <wkoller25@gmail.com>
2019-12-28 03:53:45 -0300

    adicionando connection

--- src/connection.php
+++ src/connection.php
@@ -1,13 +1,9 @@
 <?php
 
 mysqli_report(MYSQLI_REPORT_ERROR);
-//$server = 'localhost';
-//$user = 'root';
-//$pass = '210689';
-//$db = 'admin_web_php';
-$server = 'us-cdbr-iron-east-05.cleardb.net';
-$user = 'b932438974ae42';
-$pass = '5b72427';
-$db = 'heroku_9a5b49a3240fab4';
+$server = 'localhost';
+$user = 'root';
+$pass = '210689';
+$db = 'admin_web_php';
 $conn = new mysqli($server, $user, $pass, $db);
 
